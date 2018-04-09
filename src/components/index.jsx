b4f98482128dcaec47dc19b17661a0191132ee60
wyattsweet <wyattsweet@gmail.com>
2018-04-08 19:37:43 -0700

    updates to config

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from './App';
-
 import '../styles/base.css';
+
+const App = () => {
+  return <h1>HI!!!</h1>;
+};
 
 render(<App />, document.getElementById('root'));

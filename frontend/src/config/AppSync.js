0b42ba8af8a45768fab12a599f486e64b8068f1a
wyattsweet <wyattsweet@gmail.com>
2019-02-10 21:12:04 -0800

    starting backend

--- frontend/src/config/AppSync.js
+++
@@ -1,7 +0,0 @@
-export const AppSync = {
-  graphqlEndpoint:
-    'https://qqal3ou6grcgdoqfkeocsnx6em.appsync-api.us-west-2.amazonaws.com/graphql',
-  region: 'us-west-2',
-  authenticationType: 'API_KEY',
-  apiKey: 'da2-6zpk3eg4njh4xgpsh6ak6ytyzu',
-}
